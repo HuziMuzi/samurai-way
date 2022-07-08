16c7662117fde1d7c2b7451e45d297d6df880ae7
HuziMuzi <laif17860@mail.ru>
2022-07-08 10:28:24 +0300

    Revert "Revert "data transfer in index.tsx""
    This reverts commit 54af737fb4b6697e502fe543f8aabe915ccac9e8.

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,30 @@ import './App.css';
 import {Header} from "./components/Header/Header";
 import {Navbar} from "./components/Navbar/Navbar";
 import {Profile} from "./components/Profile/Profile";
-import Dialogs from "./components/Dialogs/Dialogs";
+import Dialogs, {DialogsDataType, MessageDataType} from "./components/Dialogs/Dialogs";
 import {BrowserRouter, Route} from "react-router-dom";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 import News from "./components/News/News";
+import {PostDataType} from "./components/Profile/MyPost/MyPosts";
 
-function App() {
+
+type AppPropsType = {
+    dialogs: Array<DialogsDataType>
+    messages: Array<MessageDataType>
+    posts: Array<PostDataType>
+}
+
+function App(props: AppPropsType) {
     return (
         <BrowserRouter>
             <div className='app-wrapper'>
                 <Header/>
                 <Navbar/>
                 <div className='app-wrapper-content'>
-                    <Route path={'/Profile'} component={Profile}/>
-                    <Route path={'/Dialogs'} component={Dialogs}/>
+                    <Route path={'/Profile'} render={() => <Profile posts={props.posts}/>}/>
+                    <Route path={'/Dialogs'}
+                           render={() => <Dialogs dialogs={props.dialogs} messages={props.messages}/>}/>
                     <Route path={'/Music'} component={Music}/>
                     <Route path={'/News'} component={News}/>
                     <Route path={'/Settings'} component={Settings}/>
