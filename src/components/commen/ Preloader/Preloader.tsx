8f6f5825a4a7891e197f0ca324590ba9845e9231
HuziMuzi <laif17860@mail.ru>
2022-09-26 19:46:00 +0300

    add Preloader

---
+++ src/components/commen/ Preloader/Preloader.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import preloader from "../../../img/VAyR.gif";
+
+export const Preloader = () => {
+    return (
+        <>
+            <div>
+                <img src={preloader}/>
+            </div>
+        </>
+    );
+};
+
