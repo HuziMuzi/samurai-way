f4132f9e537f6d7c229126dbfa97e766ad83f3e2
HuziMuzi <laif17860@mail.ru>
2022-12-05 17:39:07 +0300

    added styles navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,36 +1,51 @@
 import React from 'react';
-import s from './Navbar.module.css'
+import s from './Navbar.module.scss'
 import {NavLink} from "react-router-dom";
 import Sidebar from "../Sidebar/Sidebar";
 
 import {AppRootState} from "../../Redux/redux-store";
 import {initialStateTypeNavBar} from "../../Redux/navbar-reducer";
 import {useSelector} from "react-redux";
+import MyProfileIcon from "../common/icons/iconsNavbar/MyProfileIcon";
+import UsersIcon from "../common/icons/iconsNavbar/UsersIcon";
+import DialogsIcon from "../common/icons/iconsNavbar/DialogsIcon";
+import NewsIcon from "../common/icons/iconsNavbar/NewsIcon";
+import MusicIcon from "../common/icons/iconsNavbar/MusicIcon";
+import SettingsIcon from "../common/icons/iconsNavbar/SettingsIcon";
 
-export type navbarPropsType = {
 
-}
-
-export const Navbar = (props: navbarPropsType) => {
+export const Navbar = () => {
     const navBarPageState = useSelector<AppRootState, initialStateTypeNavBar>(state => state.navbarReducer)
     return (
         <>
             <nav className={s.nav}>
                 <div className={`${s.item} ${s.active}`}>
-                    <NavLink to='/Profile' activeClassName={s.activeLink}>Profile</NavLink></div>
+                    <NavLink to='/Profile' activeClassName={s.activeLink}>
+                        <MyProfileIcon/>
+                        <span> Profile </span></NavLink></div>
                 <div className={s.item}>
-                    <NavLink to="/Users" activeClassName={s.activeLink}>Users</NavLink></div>
+                    <NavLink to="/Users" activeClassName={s.activeLink}>
+                        <UsersIcon/>
+                        <span>Users </span></NavLink></div>
                 <div className={s.item}>
-                    <NavLink to="/Dialogs" activeClassName={s.activeLink}>Dialogs</NavLink></div>
+                    <NavLink to="/Dialogs" activeClassName={s.activeLink}>
+                        <DialogsIcon/>
+                        <span>Dialogs </span></NavLink></div>
                 <div className={s.item}>
-                    <NavLink to="/News" activeClassName={s.activeLink}>News</NavLink></div>
+                    <NavLink to="/News" activeClassName={s.activeLink}>
+                        <NewsIcon/>
+                        <span>News </span></NavLink></div>
                 <div className={s.item}>
-                    <NavLink to="/Music" activeClassName={s.activeLink}>Music</NavLink></div>
+                    <NavLink to="/Music" activeClassName={s.activeLink}>
+                        <MusicIcon/>
+                        <span>Music </span></NavLink></div>
                 <div className={s.item}>
-                    <NavLink to="/Settings" activeClassName={s.activeLink}>Settings</NavLink></div>
-                <div className={`${s.item}`}>
+                    <NavLink to="/Settings" activeClassName={s.activeLink}>
+                        <SettingsIcon/>
+                        <span>Settings </span></NavLink></div>
+                {/*<div className={`${s.item}`}>*/}
                     <Sidebar state={navBarPageState.sidebar}/>
-                </div>
+                {/*</div>*/}
             </nav>
         </>
     );
